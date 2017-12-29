9575f1f4ff06fe51e29c6e9c511b6a03e25922f4
root <root@ip-172-31-18-139.sa-east-1.compute.internal>
2017-12-29 12:23:14 +0000

    update rundeck

--- rundeck/prerequisites/opt/entrypoint.sh
+++ rundeck/prerequisites/opt/entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-prog="rundeckd"
+set -e
+
+initfile=/etc/rundeck.init
+
+chmod 1777 /tmp
+
+. /lib/lsb/init-functions
+. /etc/rundeck/profile
+
+PROCESS="rundeckd"
 DAEMON="${JAVA_HOME:-/usr}/bin/java"
 DAEMON_ARGS="${RDECK_JVM} -cp ${BOOTSTRAP_CP} com.dtolabs.rundeck.RunServer /var/lib/rundeck 4440"
 rundeckd="$DAEMON $DAEMON_ARGS"
 
-echo -n "Starting ${prog}"
+echo -n "Iniciando ${PROCESS}"
 su -s /bin/bash rundeck -c "$rundeckd"
