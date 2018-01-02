ca55710c665c479fcb80cfb14be6a7ff32c7bf14
root <root@ip-172-31-18-139.sa-east-1.compute.internal>
2018-01-02 01:08:12 +0000

    update codigo

--- rundeck/prerequisites/opt/entrypoint.sh
+++ rundeck/prerequisites/opt/entrypoint.sh
@@ -6,6 +6,30 @@ initfile=/etc/rundeck.init
 
 chmod 1777 /tmp
 
+#if [ ! -f "${initfile}" ]; then
+#   SERVER_URL=${SERVER_URL:-"http://0.0.0.0:4440"}
+#   HOST_DATABASE=${HOST_DATABASE:-"localhost"}
+#   DATABASE_URL=${DATABASE_URL:-"jdbc:mysql://localhost/rundeckdb?autoReconnect=true"}
+#   RUNDECK_DB_PASSWORD=${RUNDECK_PASSWORD:-$(pwgen -s 15 1)}
+#   
+#	if [ -z "$SERVER_URL" ]; then
+#		echo "Preencher SERVER_URL"
+#	else
+#		sed -i 's,grails.serverURL\=.*,grails.serverURL\='${SERVER_URL}',g' /etc/rundeck/rundeck-config.properties
+#	fi
+#
+#        if [ -z "$DATABASE_URL" ]; then
+#                echo "Preencher DATABASE_URL para conexao de banco externa"
+#        else
+#                sed -i 's,dataSource.url = .*,dataSource.url = '${DATABASE_URL}',g' /etc/rundeck/rundeck-config.properties
+#	  	 sed -i 's,dataSource.dbCreate.*,,g' /etc/rundeck/rundeck-config.properties
+#                echo "dataSource.username = rundeck" >> /etc/rundeck/rundeck-config.properties
+#                echo "dataSource.password = ${RUNDECK_DB_PASSWORD}" >> /etc/rundeck/rundeck-config.properties
+#		 echo "create database `rundeckdb`" | mysql -h $HOST_DATABASE -uroot
+#		 echo "grant replication all on *.* to rundeck@'%' identified by '${RUNDECK_DB_PASSWORD}';" | mysql -h $HOST_DATABASE -uroot 
+#        fi
+#fi
+
 . /lib/lsb/init-functions
 . /etc/rundeck/profile
 
